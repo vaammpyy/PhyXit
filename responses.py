1aa9e66733b3cf405f48328ce12c4312fa418afe
TheTarnished42 <apuroop42@gmail.com>
2023-08-13 00:15:58 +0530

    UI updated

--- responses.py
+++ responses.py
@@ -5,6 +5,8 @@ from papers import *
 
 f = open('main.txt', mode='r')
 help = f.read()
+sim_h = open('sim_h', mode='r').read()
+arxiv_h = open('arxiv_h', mode='r').read()
 
 
 def handle_response(message) -> str:
@@ -12,32 +14,34 @@ def handle_response(message) -> str:
     if p_message[0] == 'hello':
         return ['Hey there! \n>>> \n2 \n3 \n4 \n5', 't']
     if p_message[0] == 'img':
-        # while(True):
-        # try:
-        # media = ['vid.mp4', '38891d63301e957da6fc19b732a09caf.jpg']
+
         return [['movie.mp4', '38891d63301e957da6fc19b732a09caf.jpg'], 'm']
-        # except:
-        #     continue
+
     if p_message[0] == 'sim':
         try:
             os.remove('movie.mp4')
+            os.remove('op.txt')
         except:
             pass
+        if p_message[1] == '-h':
+            return ["\n>>> "+sim_h, 't']
         if p_message[1] == 'projectile':
-            v = float(p_message[2])
-            theta = float(p_message[3])
-            h = float(p_message[4])
-            dt = float(p_message[5])
-            t = float(p_message[6])
-            projectile_motion(v, theta, h, dt, t)
+            data_vec = [float(x) for x in p_message[2:]]
+            projectile_motion(*data_vec)
+            return [['movie.mp4', "op.txt"], 'm']
+        if p_message[1] == 'pendulum':
+            data_vec = [float(x) for x in p_message[2:]]
+            # pendulum function(*data_vec)
+            return [['movie.mp4', "op.txt"], 'm']
+        return ["\n>>> "+sim_h, 't']
 
-            
-            return [['movie.mp4', "\n>>> "+help], 'm']
     if p_message[0] == 'arxiv':
         try:
             os.remove('papers.log')
         except:
             pass
+        if p_message[1] == '-h':
+            return ["\n>>> "+arxiv_h, 't']
         if p_message[1] == 'top':
             query = ' '.join(p_message[2:])
             getList(query)
@@ -49,6 +53,5 @@ def handle_response(message) -> str:
             paper = open('papers.log', mode='r')
             lines = paper.read()
             return [f"Details of paper: **{p_message[2]}**\n>>> "+lines, 't']
-
-
+        return ["\n>>> "+arxiv_h, 't']
     return ["\n>>> "+help, 't']
