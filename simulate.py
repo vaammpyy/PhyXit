7657258813773aae970e988e6b1f272f71c19e27
vaammpyy <rohankumarprasad@yahoo.com>
2023-08-12 20:13:30 +0530

    removing pycache

--- simulate.py
+++ simulate.py
@@ -1,9 +1,7 @@
-import numpy as np
-import os
-
 from solver import *
 from plotter import *
 
-projectile(10,45,10,0.1,10)
-plot_projectile()
-make_movie()
+def projectile_motion():
+    projectile(10,45,10,0.1,10)
+    plot_projectile()
+    make_movie()
