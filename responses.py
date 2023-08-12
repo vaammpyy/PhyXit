1cd3f75509f4b07296835854192800a930cf67b6
Sattwamo Ghosh <sattwamoghosh@gmail.com>
2023-08-12 20:48:01 +0530

    integrated simulation

--- responses.py
+++ responses.py
@@ -1,4 +1,6 @@
 import discord
+from simulate import *
+
 def handle_response(message) -> str:
     p_message=' '.join(message).lower().split()
     if p_message[0] == 'hello':
@@ -10,4 +12,14 @@ def handle_response(message) -> str:
         return [['vid.mp4','38891d63301e957da6fc19b732a09caf.jpg'], 'm']
             # except:
             #     continue
+    if p_message[0] == 'sim':
+        if p_message[1] == 'projectile':
+            projectile_motion(10,45,5,0.1,2)
+
+            while(True):
+                try: 
+                    return [['movie.mp4'], 'm']
+                except:
+                    continue
+            
     return "IDK"
