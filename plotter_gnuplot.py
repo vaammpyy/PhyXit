c7dad6ff565600bcaed7778aecdcbbe0cf03d098
tushargayan2324 <55867762+tushargayan2324@users.noreply.github.com>
2023-08-13 09:01:49 +0530

    Add files via upload

--- plotter_gnuplot.py
+++ plotter_gnuplot.py
@@ -4,12 +4,13 @@ import pandas as pd
 
 import subprocess
 
-def make_movie():
-    cmd='ffmpeg -framerate 5 -start_number 0 -i ./%05d.jpeg -vcodec mpeg4 -vb 20M '+'movie.mp4'
+def make_movie_gnuplot():
+    #cmd = "ffmpeg -framerate 20 -pattern_type  glob -i /output/*.png c_change_bif.mp4"
+    cmd = "ffmpeg -framerate 10 -pattern_type glob -i 'output/*.png' -c:v libx264 -pix_fmt yuv420p out.mp4"
     os.system(cmd)
-    os.system("rm -r ./*.jpeg")
+    os.system("rm -r output/*.png")
 
-def plot_projectile():
+def plot_projectile_gnuplot():
     data = pd.read_csv('data.txt', delimiter=' ').to_numpy().T
     frames=data[0]-1
     t=data[1]
@@ -18,51 +19,60 @@ def plot_projectile():
     vx=data[4]
     vy=data[5]
 
-    #os.system("mkdir output_plot")
+    os.system("mkdir output")
+
+
 
 
     for frame in frames:
         os.system("awk 'FNR == %d {print $0}' data.txt > outputfile"%(int(frame)))
         cmd="""
 gnuplot <<- EOF
-set terminal jpeg size 1200,1000
-set output "{:05d}.jpeg"
-
-set multiplot title "Plot_something_something"
+set terminal png size 1200,1000
+set output "output/{:05d}.png"
+set multiplot title "Projectile Motion"
 set nokey
 
 set origin 0.0, 0.5
 set size 1.0, 0.45
-plot "data.txt" using 3:4 with lines lw 3 lc 22 title 'Position', \
+set title "Position of the particle"
+set xlabel "x-axis"
+set ylabel "y-axis"
+plot "data.txt" using 3:4 with lines lw 2.5 lc 22 title 'Position', \
     "outputfile" using 3:4 pt 7 ps 3
 
+unset xlabel
+unset ylabel
 set origin 0.0, 0.0
 set size 0.25, 0.45
-plot "data.txt" using 2:3 with lines lw 3 lc 22 title 'X vs T' , \
+set title "X position vs time"
+plot "data.txt" using 2:3 with lines lw 2.5 lc 22 title 'X vs T' , \
     "outputfile" using 2:3 pt 7 ps 3
 
 set origin 0.25, 0.0
 set size 0.25, 0.45
-plot "data.txt" using 2:4 with lines lw 3 lc 22 title 'Y vs T' , \
+set title "Y position vs time"
+plot "data.txt" using 2:4 with lines lw 2.5 lc 22 title 'Y vs T' , \
     "outputfile" using 2:4 pt 7 ps 3
 
 set origin 0.5, 0.0
 set size 0.25, 0.45
-plot "data.txt" using 2:5 with lines lw 3 lc 22 title 'Vx vs T' , \
+set title "Velocity along x-axis vs time"
+plot "data.txt" using 2:5 with lines lw 2.5 lc 22 title 'Vx vs T' , \
     "outputfile" using 2:5 pt 7 ps 3
 
 set origin 0.75, 0.0
 set size 0.25, 0.45
-plot "data.txt" using 2:6 with lines lw 3 lc 22 title 'Vy vs T' , \
+set title "Velocity along y-axis vs time"
+plot "data.txt" using 2:6 with lines lw 2.5 lc 22 title 'Vy vs T' , \
     "outputfile" using 2:6 pt 7 ps 3
 
 unset multiplot
 EOF
-
-""".format(int(frame))
+    """.format(int(frame))
         os.system(cmd)
         
 
 
-plot_projectile()
-make_movie()
+plot_projectile_gnuplot()
+make_movie_gnuplot()
